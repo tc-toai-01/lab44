d61c06630655ab731d55225484fd38b2890b6948
Bepissss <19521585@gmail.uit.edu.vn>
2022-05-24 13:32:28 +0700

    hoàn chỉnh lấy data

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const mqtt = require("mqtt");
+const { stringify } = require("nodemon/lib/utils");
 
-const broker = "mqtt:172.31.250.212:1883";
+const broker = "mqtt:broker.hivemq.com:1883";
 const client = mqtt.connect(broker);
 
 client.on("connect", () => {
-  const topic = "dht/value";
-  client.subscribe(topic);
+  const topic = "nhom10/dht/temp";
+  const topic2 = "nhom10/dht/humi";
+  const topic3 = "nhom10/dht/lux";
+  const topic4 = "nhom10/dht/led1";
+  const topic5 = "nhom10/dht/led2";
+  client.subscribe([topic,topic3,topic4,topic5,topic2]);
 });
 
-client.on("message", (topic, message) => {
-  console.log(message.toString());
-  
+client.on("message", (topic,  message) => {
+  let m = message.toString();
+  const type = m.split("/");
+  let name = type[0];
+  var value = type[1];
+  if (name == "tem"){
+    console.log("tempurate: " + value)
+  }
+  if (name == "hum"){
+    console.log("humidity: " + value)
+  }
+  if (name == "lux"){
+    console.log("luxuri: " + value)
+  }  
 });
